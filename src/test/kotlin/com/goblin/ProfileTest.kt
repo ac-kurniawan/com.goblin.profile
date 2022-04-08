12c73259f7395f9a8a1c60db0efca437410225cb
ac-kurniawan <ac.kurniawan99@gmail.com>
2022-04-09 01:12:58 +0700

    update UT

--- src/test/kotlin/com/goblin/ProfileTest.kt
+++ src/test/kotlin/com/goblin/ProfileTest.kt
@@ -1,18 +1,29 @@
 package com.goblin
+import com.goblin.utils.PostgresContainer
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 @MicronautTest
 class ProfileTest {
-
+    companion object {
+        val psqlContainer = PostgresContainer("postgres:12-alpine").apply {
+            withDatabaseName("postgres")
+            withUsername("username")
+            withPassword("password")
+            withInitScript("migrations/V1__create-schema.sql")
+        }
+    }
     @Inject
     lateinit var application: EmbeddedApplication<*>
 
     @Test
     fun testItWorks() {
+        psqlContainer.start()
         Assertions.assertTrue(application.isRunning)
     }
 }
