307f3d2fcbe78d0ece798e1aa37e77622931915f
ac-kurniawan <ac.kurniawan99@gmail.com>
2022-04-09 00:40:47 +0700

    update UT

--- src/test/kotlin/com/goblin/core/impl/ProfileServiceImplTest.kt
+++ src/test/kotlin/com/goblin/core/impl/ProfileServiceImplTest.kt
@@ -3,10 +3,13 @@ package com.goblin.core.impl
 import com.goblin.core.Profile
 import com.goblin.core.ProfileProducer
 import com.goblin.core.ProfileRepository
+import com.goblin.utils.PostgresContainer
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -15,6 +18,12 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @MicronautTest(rollback = false, transactional = false)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProfileServiceImplTest {
+    companion object {
+        val psqlContainer = PostgresContainer("postgres:12-alpine").apply {
+            withDatabaseName("postgres")
+            withInitScript("migrations/V1__create-schema.sql")
+        }
+    }
 
     @Inject
     lateinit var profileRepository: ProfileRepository
@@ -24,6 +33,16 @@ class ProfileServiceImplTest {
 
     @Inject
     lateinit var profileServiceImpl: ProfileServiceImpl
+
+    @BeforeAll
+    fun beforeAll() {
+        psqlContainer.start()
+    }
+
+    @AfterAll
+    fun afterAll() {
+        psqlContainer.stop()
+    }
 
     @Test
     fun `getProfileById return profile data when success`() {
